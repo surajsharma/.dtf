7b447c068ade70ee139f3c63b6f57053c0e622d0
Suraj Sharma <www.surajsharma@gmail.com>
2018-04-18 07:11:30 +0530

    refresh

--- py/class.py
+++
@@ -1,6 +0,0 @@
-"""To illustrate these ideas, this section will now explore the code
-for the definition of a Counter class. A counter object, as the name
-implies, tracks an integer count. A counter’s value is initially 0 and
-can be reset to 0 at any time. You can increment or decrement a
-counter, obtain its current integer value, obtain its string
-representation, or compare two counters for equality"""
