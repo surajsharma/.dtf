72665d567e1b9bfb7981e5508fa850aef67b407b
suraj sharma <suraj@evenzero.in>
2025-09-29 16:56:11 +0530

    fix: wezterm+nvim

--- nixos/home/suraj/.config/nvim/lua/plugins/lsp.lua
+++ nixos/home/suraj/.config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
   },
   config = function()
     -- Your existing config
@@ -72,6 +72,7 @@ return {
         -- cmd = { "clangd", "--compile-commands-dir=build" },
       })
     end
+    lspconfig.tsserver.setup({})
     lspconfig.gopls.setup({
       settings = {
         gopls = {
--- nixos/home/suraj/.config/wezterm/wezterm.lua
+++ nixos/home/suraj/.config/wezterm/wezterm.lua
@@ -1,6 +1,11 @@
 local wezterm = require("wezterm")
-
 local config = {}
+local gpus = wezterm.gui.enumerate_gpus()
+
+config.enable_wayland = true
+config.front_end = "WebGpu"
+config.webgpu_preferred_adapter = gpus[1]
+config.webgpu_power_preference = "HighPerformance"
 
 -- Use config builder object if available (newer versions)
 if wezterm.config_builder then
@@ -11,7 +16,7 @@ end
 config.initial_rows = 50
 config.initial_cols = 120
 config.window_decorations = "NONE"
-config.window_background_opacity = 0.88
+config.window_background_opacity = 0.8
 config.text_background_opacity = 1.0
 
 -- Force window to be borderless and undecorated
@@ -23,16 +28,21 @@ wezterm.on("gui-startup", function(cmd)
 	window:gui_window():maximize()
 end)
 
+-- command pallete colors
+config.command_palette_bg_color = "rgba(0, 0, 0, 0.8)"
+config.command_palette_fg_color = "rgba(150,255,0,0.8)"
+config.command_palette_font = wezterm.font("Berkeley Mono")
+config.font = wezterm.font("Berkeley Mono")
+config.command_palette_font_size = 15.0
+config.command_palette_rows = 10
+
 -- Alternative: set window to fullscreen if maximize doesn't work
 -- wezterm.on('gui-startup', function(cmd)
 --   local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
 --   window:gui_window():set_inner_size(9999, 9999)
 -- end)
 
--- Color scheme - using Tokyo Night theme
 -- config.color_scheme = 'Tokyo Night'
-
--- Alternative dark themes you can try:
 -- config.color_scheme = 'Dracula'
 -- config.color_scheme = 'OneDark'
 -- config.color_scheme = 'Gruvbox Dark (Gogh)'
@@ -41,9 +51,41 @@ end)
 -- config.color_scheme = "Kanagawa (Gogh)"
 -- config.color_scheme = "Kanagawa Dragon (Gogh)"
 -- config.color_scheme = "Konsolas"
-config.color_scheme = "Sex Colors (terminal.sexy)"
 -- config.color_scheme = "Sundried"
 -- config.color_scheme = "SOS (terminal.sexy)"
+config.color_scheme = "Sex Colors (terminal.sexy)"
+config.colors = {
+	tab_bar = {
+		-- The color of the strip that goes along the top of the window
+		-- (does not apply when fancy tab bar is in use)
+		background = "transparent",
+
+		inactive_tab = {
+			bg_color = "transparent",
+			fg_color = "#808080",
+		},
+
+		inactive_tab_hover = {
+			bg_color = "transparent",
+			fg_color = "#80ff80",
+		},
+
+		active_tab = {
+			bg_color = "transparent",
+			fg_color = "#ff8080",
+		},
+
+		new_tab = {
+			bg_color = "transparent",
+			fg_color = "#80ff80",
+		},
+
+		new_tab_hover = {
+			bg_color = "#008000",
+			fg_color = "#80ff80",
+		},
+	},
+}
 
 -- Font configuration
 config.font = wezterm.font("Berkeley Mono")
@@ -56,7 +98,7 @@ config.cursor_blink_rate = 500
 -- Tab bar configuration
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = false
+config.tab_bar_at_bottom = true
 
 -- Scrollback
 config.scrollback_lines = 10000
