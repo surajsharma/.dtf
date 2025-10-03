6653cdadd9118e9a44ed415918137e75f50bab43
suraj sharma <suraj@evenzero.in>
2025-10-04 00:00:48 +0530

    fix: nvim clojure support

--- nixos/home/suraj/.config/nvim/lua/plugins/lsp.lua
+++ nixos/home/suraj/.config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,37 @@ return {
   config = function()
     -- Your existing config
     local lspconfig = require("lspconfig")
+    local util = require("lspconfig.util")
+
+    -- Clojure
+    lspconfig.clojure_lsp.setup({
+      -- If Mason's PATH isn't picked up (e.g. on Nix), use the absolute Mason binary:
+      cmd = { vim.fn.stdpath("data") .. "/mason/bin/clojure-lsp" },
+      filetypes = { "clojure", "clojurescript", "cljc", "edn" },
+      root_dir = util.root_pattern("deps.edn", "project.clj", "shadow-cljs.edn", "bb.edn", ".git"),
+      on_attach = function(client, bufnr)
+        -- format on save (cljfmt via clojure-lsp; configure in .lsp/config.edn)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 2000 })
+            end,
+          })
+        end
+
+        -- optional: organize imports/clean-ns on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.code_action({
+              context = { only = { "source.organizeImports" } },
+              apply = true,
+            })
+          end,
+        })
+      end,
+    })
 
     -- This will override Mason's setup
     lspconfig.lua_ls.setup({
